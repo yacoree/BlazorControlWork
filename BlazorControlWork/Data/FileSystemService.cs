1bd5c1bcc0ff3b69729c9fda5daad6ac0833ec83
Yuri Lifanov <yuri.li.urta2210@gmail.com>
2022-11-02 23:09:04 +0300

    try save img

---
+++ BlazorControlWork/Data/FileSystemService.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Driver;
+using MongoDB.Driver.GridFS;
+
+namespace BlazorControlWork.Data
+{
+    public class FileSystemService
+    {
+        public void UploadImageToDb()
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("Images321");
+            var gridFS = new GridFSBucket(database);
+
+            using (FileStream fs = new FileStream("C:\\Users\\vglif\\Pictures\\Roblox/RobloxScreenShot20221014_211523792.png", FileMode.Open))
+            {
+                gridFS.UploadFromStream("sss.jpg", fs);
+            }
+        }
+
+        public void DownloadToLocal()
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("Images321");
+            var gridFS = new GridFSBucket(database);
+            using (FileStream fs = new FileStream($"{Directory.CreateDirectory(Directory.GetCurrentDirectory() + "/Images/")}DeserializedBall.jpg", FileMode.CreateNew))
+            {
+                gridFS.DownloadToStreamByName("sss.jpg", fs);
+            }
+        }
+    }
+}
--- BlazorControlWork/Program.cs
+++ BlazorControlWork/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<SingletonUserService>();
+builder.Services.AddSingleton<FileSystemService>();
 
 var app = builder.Build();
 
